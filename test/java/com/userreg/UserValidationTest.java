5e51d134b2a47a39fcfc8129cebe3159fa0d6279
SweetyLenka <55683708+SweetyLenka@users.noreply.github.com>
2021-04-05 17:22:47 +0530

    Update UserValidationTest.java
    Updated UC4

--- test/java/com/userreg/UserValidationTest.java
+++ test/java/com/userreg/UserValidationTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class UserValidationTest {
+    
+     UserValidator validator = new UserValidator();
 
     @Test
     public void isFirstNameValid()
     {
-        UserValidator validator = new UserValidator();
         boolean result=validator.validateFirstName("Sweety");
         Assertions.assertEquals(true,result);
 
@@ -16,7 +17,6 @@ public class UserValidationTest {
     @Test
     public void isLastNameValid()
     {
-        UserValidator validator = new UserValidator();
         boolean result=validator.validateLastName("Das");
         Assertions.assertEquals(true,result);
 
@@ -24,20 +24,13 @@ public class UserValidationTest {
     @Test
     public void isEmailValid()
     {
-        UserValidator validator = new UserValidator();
         boolean result=validator.validateEmail("sweetylenka2013@gmail.com");
         Assertions.assertEquals(true,result);
     }
     @Test
     public void isMobileValid()
     {
-        UserValidator validator = new UserValidator();
         boolean result=validator.validateMobile("91 1234567890");
         Assertions.assertEquals(true,result);
     }
-
-
-
-
-
 }
