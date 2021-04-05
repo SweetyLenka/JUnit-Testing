72f7f93c03b81fcb02f95d7c44482416999a576c
sweetylenka <sweetylenka2013@gmail.com>
2021-04-05 17:07:32 +0530

    Updated : UC1 - Writing a test case to check whether First Name is valid

--- main/java/com/userreg/UserValidator.java
+++ main/java/com/userreg/UserValidator.java
@@ -1,38 +1,14 @@
 package com.userreg;
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserValidator {
     private static final String FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-    private static final String LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
-    private static final String EMAIL_PATTERN="^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
-    private static final String MOBILE_PATTERN="[9]{1}[1]{1}[\s][0-9]{10}";
-    private static final String PASSWORD_PATTERN="^(?=.*[A-Z])(?=.*\\\\d)(?=.*[@$!%*#?&])[A-Za-z0-9$&+,:;=?@#|'<>.^*()%!-]{8,}$";
 
     public boolean validateFirstName(String fname) {
         Pattern pattern= Pattern.compile(FIRST_NAME_PATTERN);
         return pattern.matcher(fname).matches();
     }
-
-
-    public boolean validateLastName(String lname) {
-        Pattern pattern= Pattern.compile(LAST_NAME_PATTERN);
-        return pattern.matcher(lname).matches();
-    }
-
-    public boolean validateEmail(String email) {
-        Pattern pattern= Pattern.compile(EMAIL_PATTERN);
-        return pattern.matcher(email).matches();
-    }
-
-    public boolean validateMobile(String mobile) {
-        Pattern pattern=Pattern.compile(MOBILE_PATTERN);
-        return pattern.matcher(mobile).matches();
-    }
-
-    public boolean validatePassword(String password) {
-        Pattern pattern=Pattern.compile(PASSWORD_PATTERN);
-        return pattern.matcher(password).matches();
-
-    }
+    
 }
--- test/java/com/userreg/UserValidationTest.java
+++ test/java/com/userreg/UserValidationTest.java
@@ -1,51 +1,16 @@
 package com.userreg;
-
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 public class UserValidationTest {
+    UserValidator validator = new UserValidator();
+    boolean result;
 
     @Test
     public void isFirstNameValid()
     {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validateFirstName("Sweety");
-        Assertions.assertEquals(true,result);
+        result=validator.validateFirstName("Sweety");
+        assertEquals(true,result);
 
     }
-    @Test
-    public void isLastNameValid()
-    {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validateLastName("Das");
-        Assertions.assertEquals(true,result);
-
-    }
-    @Test
-    public void isEmailValid()
-    {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validateEmail("sweetylenka2013@gmail.com");
-        Assertions.assertEquals(true,result);
-    }
-    @Test
-    public void isMobileValid()
-    {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validateMobile("91 1234567890");
-        Assertions.assertEquals(true,result);
-    }
-    @Test
-    public void isPasswordValid()
-    {
-        UserValidator validator = new UserValidator();
-        boolean result=validator.validatePassword("Sweety@2020");
-        Assertions.assertEquals(true,result);
-    }
-
-
-
-
-
-
 }
